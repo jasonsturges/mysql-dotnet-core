64e6fa0abb01950362c150ff1ed0b4f1d1c07ed5
Jason Sturges <jason@jasonsturges.com>
2022-12-29 00:48:28 -0600

    .NET Core 7.0 project scaffolding bootstrapped by Visual Studio 17.4
    ASP.NET Core Web App MVC with individual account authentication

--- MySqlDotnetCore/Controllers/HomeController.cs
+++ MySqlDotnetCore/Controllers/HomeController.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using MySqlDotnetCore.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MySqlDotnetCore.Controllers
 {
--- MySqlDotnetCore/Data/ApplicationDbContext.cs
+++ MySqlDotnetCore/Data/ApplicationDbContext.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MySqlDotnetCore.Data
 {
@@ -12,32 +8,6 @@ namespace MySqlDotnetCore.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
-        
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            builder.Entity<IdentityRole>(entity => entity.Property(m => m.Id).HasMaxLength(450));
-            
-            builder.Entity<IdentityUserLogin<string>>(entity =>
-            {
-                entity.Property(m => m.LoginProvider).HasMaxLength(127);
-                entity.Property(m => m.ProviderKey).HasMaxLength(127);
-            });
-            
-            builder.Entity<IdentityUserRole<string>>(entity =>
-            {
-                entity.Property(m => m.UserId).HasMaxLength(127);
-                entity.Property(m => m.RoleId).HasMaxLength(127);
-            });
-            
-            builder.Entity<IdentityUserToken<string>>(entity =>
-            {
-                entity.Property(m => m.UserId).HasMaxLength(127);
-                entity.Property(m => m.LoginProvider).HasMaxLength(127);
-                entity.Property(m => m.Name).HasMaxLength(127);
-            });
         }
     }
 }
